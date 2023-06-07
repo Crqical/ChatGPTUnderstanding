0f4cba9c10a80284615c28a39f3dac4c30f6cc4b
Crqical <104926518+Crqical@users.noreply.github.com>
2023-06-07 16:11:26 +0000

    COMMITER

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php
-require_once 'vendor/autoload.php';
+<?php session_start();
 
-session_start();
+require_once 'vendor/autoload.php';
 
 $client = new Google_Client();
 $client->setAuthConfig('client_secrets.json');  // Path to your client_secrets.json file
@@ -12,7 +11,6 @@ $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
     $client->setAccessToken($_SESSION['access_token']);
     $driveService = new Google_Service_Drive($client);
-    $docsService = new Google_Service_Docs($client);
 
     // Query to get Google Documents owned by the user
     $optParams = array(
@@ -22,127 +20,101 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 
     $results = $driveService->files->listFiles($optParams);
 
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>GradeFlow Test</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<a href="index.html" class="logo"><strong>GradeFlow</strong> <span>Test</span></a>
+						<nav>
+							
+						</nav>
+					</header>
+
+			
+
+				<!-- Banner -->
+					<section id="banner" class="major">
+						<div class="inner">
+							<header class="major">
+								<h1>Your Name is <?php 
+   $userInfo = $driveService->about->get(['fields' => 'user']);
+        $googleUsername = $userInfo->getUser()->getDisplayName();
+
+        echo " " . $googleUsername . "<br><br>";
+  ?></h1>
+							</header>
+							<div class="content">
+								<p>PHP Function that uses Google Drive/Docs to look through Google Document ID. Determine if cheating occurs using a multi step process. Created by Eibil </p>
+								<ul class="actions">
+						
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Main -->
+					<div id="main">
+
+			
+
+						<!-- Two -->
+							<section id="two">
+								<div class="inner">
+									<header class="major">
+										<h2>Documents</h2>
+									</header>
+									<p><?php
+
+
+
+
+
+
+
+
+
     if (count($results->getFiles()) == 0) {
         print "No Google Documents found.\n";
     } else {
-        $userInfo = $driveService->about->get(['fields' => 'user']);
-        $googleUsername = $userInfo->getUser()->getDisplayName();
-
-        echo "Hello : " . $googleUsername . "<br><br>";
-
-        print "Google Documents:<br>";
-        foreach ($results->getFiles() as $file) {
-            $counting = 0; // Reset counting for each document
-            printf("Document: %s (%s)<br>", $file->getName(), $file->getId());
-
-            // Get the document content
-            $document = $docsService->documents->get($file->getId());
-            $content = $document->getBody()->getContent();
-
-            // Process the document content
-            $text = '';
-            $headings = false;
-
-          
-          $images = false;
-$imageCount = 0;
-foreach ($content as $element) {
-    if ($element->getParagraph()) {
-        foreach ($element->getParagraph()->getElements() as $paragraphElement) {
-            if ($paragraphElement->getInlineObjectElement() || ($paragraphElement->getTextRun() && strpos($paragraphElement->getTextRun()->getContent(), '<img') !== false)) {
-                $images = true;
-                $imageCount++;
-                break;
-            }
-        }
-    }
-}
-
-          
-            foreach ($content as $element) {
-                if ($element->getParagraph()) {
-                    foreach ($element->getParagraph()->getElements() as $element) {
-                        if ($element->getTextRun()) {
-                            $text .= $element->getTextRun()->getContent();
-                        }
-                        $headings = true;
-                    }
-                  
-                }
-            }
-
-
-
-          
-$titleExists = false;
-foreach ($content as $element) {
-    if ($element->getParagraph() && $element->getParagraph()->getParagraphStyle()) {
-        $paragraphStyle = $element->getParagraph()->getParagraphStyle();
-        if ($paragraphStyle->getNamedStyleType() === 'TITLE') {
-            $titleExists = true;
-            break;
-        }
-    }
-}        
-            // Count the words
-            $wordCount = str_word_count($text);
-
-            // Display word count, headings, and image count
-            echo "Total number of words: " . $wordCount . "<br>";
-          echo "Number of images: " . $imageCount . "<br>";
-
-            if ($wordCount < 100) {
-                echo "There needs to be at least 100 words, you have " . $wordCount . "<br>";
-            } else {
-                $counting++;
-            }
-
-            // Check for headings
-            if ($headings) {
-                $counting++;
-            } else {
-                echo "THERE IS NO HEADING<br>";
-            }
-
-            // Check for images
-            if ($images) {
-                $counting++;
-            } else {
-                echo "You do not have an image<br>";
-            }
-
-
-          
-            // Check for a specific word
-            if (strpos($text, 'benrud') !== false) {
-                $counting++;
-            } else {
-                echo "The specific word 'benrud' is not found in the document<br>";
-            }
-
-          if ($titleExists) {
-    $counting++;
-} else {
-    echo "There is no title in the document<br>";
-}
-
-
-          
-            echo '<label for="file"></label>
-<progress id="file" value="'.$counting.'" max="5">32%</progress>';
-
-          If($counting == 5) {
-            echo "A+ FOR YOU. You are SMART BOI";
-          } else {
-            echo "YOU DO NOT GET a A+ GET BETTER LOOSER";
-          }
-
-          
-            echo "<hr>"; // Add a horizontal rule
-
-          
        
 
+     
+        foreach ($results->getFiles() as $file) {
+            printf("Document: %s (%s)<br>", $file->getName(), $file->getId());
+
+echo "<a href='keepers.php?url=" . $file->getId() . "' class='button next'>Analyze</a>";
+
+            // $revisions = $driveService->revisions->listRevisions($file->getId());
+            // foreach ($revisions->getRevisions() as $revision) {
+            //     $modTime = $revision->getModifiedTime();
+            //     echo "Revision ID: " . $revision->getId() . ", Modified Time: " . $modTime . "<br>";
+            // }
+
+        
+
+            echo "<hr>"; // Add a horizontal rule
         }
     }
 } else {
@@ -150,3 +122,56 @@ foreach ($content as $element) {
     header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 ?>
+</p>
+									<ul class="actions">
+									</ul>
+								</div>
+							</section>
+
+					</div>
+
+				<!-- Contact -->
+					<section id="contact">
+						<div class="inner">
+							<section>
+								
+										<div class="field half">
+											<label for="name">Document ID/URL:</label>
+											<input type="text" name="name" id="name" />
+										</div><br>
+									
+									<ul class="actions">
+										<li><input type="submit" value="Inspect" class="primary" /></li>
+									</ul>
+								</form>
+							
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<ul class="icons">
+								<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ process.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use PhpOffice\PhpWord\IOFactory;
+
+session_start();
+
+// Check that both the URL and docName are set
+if (isset($_GET['id']) && isset($_SESSION['docName'])) {
+    $docId = $_GET['id'];
+    $docName = $_SESSION['docName'];
+
+    // Retrieve all .docx files in the directory
+    $docxFiles = glob('*.docx');
+
+    // Find the files that match the $docName pattern
+    $matchingFiles = preg_grep('/^' . preg_quote($docName, '/') . '\s*\d*\.docx$/', $docxFiles);
+
+    // Create an array to hold the file names, content and revision times
+    $fileData = array();
+
+    // Add the current document to the array
+    $currentFile = $docName . ' CURRENT.docx';
+    if (file_exists($currentFile)) {
+        $phpWord = IOFactory::load($currentFile);
+        $content = '';
+        foreach ($phpWord->getSections() as $section) {
+            $elements = $section->getElements();
+            foreach ($elements as $element) {
+                if (get_class($element) === 'PhpOffice\PhpWord\Element\TextRun') {
+                    foreach ($element->getElements() as $text) {
+                        if (method_exists($text, 'getText')) {
+                            $content .= $text->getText();
+                        }
+                    }
+                }
+            }
+        }
+
+        $fileData[] = array(
+            'name' => $currentFile,
+            'content' => $content,
+            'revisionTime' => 'CURRENT'
+        );
+    }
+
+    // Add the matching file names, their content and revision times to the array
+    if (count($matchingFiles) > 0) {
+        foreach ($matchingFiles as $file) {
+            $phpWord = IOFactory::load($file);
+            $content = '';
+            foreach ($phpWord->getSections() as $section) {
+                $elements = $section->getElements();
+                foreach ($elements as $element) {
+                    if (get_class($element) === 'PhpOffice\PhpWord\Element\TextRun') {
+                        foreach ($element->getElements() as $text) {
+                            if (method_exists($text, 'getText')) {
+                                $content .= $text->getText();
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Extract the revision number from the file name
+            $revisionNumber = (int) substr($file, strlen($docName) + 1, -5);
+
+            $fileData[] = array(
+                'name' => $file,
+                'content' => $content,
+                'revisionTime' => $_SESSION['revisionTimes'][$revisionNumber] ?? ''
+            );
+        }
+    }
+
+    // Convert the array to JSON
+    $json = json_encode($fileData, JSON_PRETTY_PRINT);
+
+    // Write the JSON to a file
+    $file = $_SESSION['docName'] . '.json';
+    file_put_contents($file, $json);
+
+    // Redirect to checker.php
+    header('Location: checker.php?id='.$docId);
+    exit();  // Add this line
+}
+?>
